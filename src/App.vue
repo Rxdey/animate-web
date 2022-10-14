4eb823296aaa129a2851f93c1a8764d48f6703af
rxdey <jyh1994@qq.com>
2022-10-14 16:12:41 +0800

    🌈 style(主色调): style

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,61 @@
 <template>
   <router-view class="page"></router-view>
+  <nav class="nav-bar">
+    <div class="nav-list">
+      <div class="nav-bar__item">Home</div>
+      <div class="nav-bar__item">User</div>
+    </div>
+  </nav>
 </template>
 
 <script setup lang="ts"></script>
 
 <style lang="less">
+html,
+body {
+  height: 100%;
+  overflow: hidden;
+}
 #app {
   font-size: 28px;
+  max-width: 10rem;
+  margin: 0 auto;
+  display: flex;
+  flex-flow: column;
+  height: 100%;
+}
+.page {
+  flex: 1;
+}
+.nav-bar {
+  height: 110px;
+  position: relative;
+  color: var(--color-white);
+  text-shadow: 2px 4px 0 var(--color-origin);
+  .nav-list {
+    display: flex;
+    flex-flow: row nowrap;
+    background-color: var(--color-brown);
+    border-radius: 100px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    box-shadow: 4px 4px 2px var(--color-yellow);
+    border-bottom: 2px solid var(--color-purple);
+    border-right: 2px solid var(--color-purple);
+  }
+  &__item {
+    text-align: center;
+    flex: 1;
+    padding: 8px 32px;
+    width: 160px;
+    &:first-child {
+      border-right: 2px solid var(--color-bg);
+    }
+    &:last-child {
+      border-left: 2px solid var(--color-bg);
+    }
+  }
 }
 </style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="home">
-    车险
+    <div class="banner"></div>
+    <div class="tab-tag">
+      <div class="tab-item shadow active">书架</div>
+      <div class="tab-item shadow">看过</div>
+    </div>
+    <div class="comic-list">
+      <div class="comic-card shadow"></div>
+      <div class="comic-card shadow"></div>
+      <div class="comic-card shadow"></div>
+      <div class="comic-card shadow"></div>
+    </div>
   </div>
 </template>
 
@@ -10,4 +20,51 @@ import { useRouter, useRoute } from 'vue-router';
 </script>
 
 <style lang="less" scope>
+.banner {
+  height: 450px;
+  margin: -40px 0 16px 0;
+  background-color: var(--color-yellow);
+  transform: skewY(2deg);
+  background-image: url('@/assets/img/banner.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  // box-shadow: var(--color-yellow);
+  box-shadow: 0 8px 0 var(--color-yellow);
+  border-bottom: 4px solid var(--color-purple);
+}
+.tab-tag {
+  padding: 16px 32px;
+  display: flex;
+  flex-flow: row nowrap;
+  margin-bottom: 16px;
+}
+.tab-item {
+  width: 33%;
+  text-align: center;
+  font-size: var(--font-xl);
+  line-height: 1.2;
+  text-shadow: 2px 4px 0 var(--color-origin);
+  &.active {
+    background-color: var(--color-purple);
+    color: var(--color-white);
+  }
+  &+.tab-item {
+    margin-left: 32px;
+  }
+}
+.comic-list {
+  --column-grap: 36px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, calc(33.33% - var(--column-grap)));
+  justify-content: center;
+  row-gap: 24px;
+  column-gap: var(--column-grap);
+  padding: 0 16px;
+}
+.comic-card {
+  height: 280px;
+  // border-radius: 16px;
+}
 </style>
